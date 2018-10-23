dfb22d2d3b901e74413ef0598a33ce31cf372da3
Yingying Wang <35354872+yingyingww@users.noreply.github.com>
2018-10-22 21:06:59 -0500

    Update and rename test.py to extractBySoup.py

---
+++ extractBySoup.py
@@ -0,0 +1,38 @@
+##need to install:
+# pip install beautifulsoup4
+# pip install lxml
+# pip install requests 
+# pip install html5lib
+
+# work to do - 
+## currently can extract from a url
+## but prints <p> within the texts...
+
+
+from bs4 import BeautifulSoup
+
+import requests
+
+url = input("Enter a website to extract the URL's from: ")
+
+r  = requests.get(url)
+
+data = r.text
+
+soup = BeautifulSoup(data)
+#print( soup.prettify())
+headline = soup.title.string
+print("title is ",soup.title.string)
+#print("title is ")
+#print(soup.get_text())
+
+#print(soup.find_all("p"))
+
+
+#http://print("title is ",soup.title.string)
+#for link in soup.find_all('a'):
+#    print(link.get('href'))
+content = soup.find_all('p')
+#title = soup.find_all('h2')
+#print(title)
+print(content)
