4aacc10ab792d1528554bbbb9410c068c7ade766
Tianna Avery <tiannaavery@eduroam-161-35.dyn.carleton.edu>
2018-11-16 02:22:41 -0600

    Sentiment analysis using TextBlob and similarity to role function

--- chrome-extension-testing/math_example.py
+++
@@ -1,9 +0,0 @@
-def sent_tokenize(text, language='english'):
-    tokenizer = load('tokenizers/punkt/{0}.pickle'.format(language))
-    return tokenizer.tokenize(text)
-
-
-class TokenizerI(object):
-    def tokenize(self, s):
-        if overridden(self.tokenize_sents):
-            return self.tokenize_sents([s])[0]
