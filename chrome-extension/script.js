592d1c555412061b9d939bd0c143a2cc4ed4e756
Quinn Mayville <mayvilleq@carleton.edu>
2019-01-21 19:56:20 -0600

    added loading message

--- chrome-extension/script.js
+++ chrome-extension/script.js
@@ -1,9 +1,14 @@
 var __name__ = "__main__";
 var run;
 run = function(tabs) {
+    localhost = "http://127.0.0.1:5000/";
     var url;
     url = tabs[0].url;
-    localhost = "http://127.0.0.1:5000/";
+
+    // Hide everything except loading message (and title)
+    $('div:not(#loading)').hide();
+    $('div#title').show();
+
     $(document).ready(function() {
         $.get(localhost, {"url": url}, function(data){
             // document.getElementById("loading").innerHTML = "";
@@ -13,6 +18,10 @@ run = function(tabs) {
             document.getElementById("heroDiv").querySelectorAll("#hero")[0].innerHTML = data[0];
             document.getElementById("villainDiv").querySelectorAll("#villain")[0].innerHTML = data[1];
             document.getElementById("victimDiv").querySelectorAll("#victim")[0].innerHTML = data[2];
+
+            // Show everything except loading message
+            $('div').hide();
+            $('div:not(#loading)').show();
         });
     });
 };
