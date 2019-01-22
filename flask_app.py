5ef273382e8f754cc9a6bcbdde8a1274b23bf59a
Quinn Mayville <mayvilleq@carleton.edu>
2019-01-21 20:00:41 -0600

    refactor

--- flask_app.py
+++ flask_app.py
@@ -20,11 +20,7 @@ def top_entities():
     headline, article = role_assignment.extract_by_newsplease(url)
     top_entities = entity_recognition.get_top_entities(headline, article)
     top_entity_names = [entity.name for entity in top_entities]
-    print(top_entity_names)
-    # return tuple(top_entities_name)
-    # return "\n".join(top_entity_names)
     return jsonify(top_entity_names)
-    # return render_template('results.html', top_entities_name = top_entities_name)
 
 
 if __name__ == '__main__':
