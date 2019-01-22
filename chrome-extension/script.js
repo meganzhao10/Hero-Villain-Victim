8accaf3d1a6c288c5aafec6ab0f831ab3d401ace
Quinn Mayville <mayvilleq@carleton.edu>
2019-01-21 19:33:38 -0600

    updated data feed

--- chrome-extension/script.js
+++ chrome-extension/script.js
@@ -4,14 +4,17 @@ run = function(tabs) {
     var url;
     url = tabs[0].url;
     localhost = "http://127.0.0.1:5000/";
-    // $(document).ready(function() {
-    //     $.get(localhost, {"url": url}, function(data){
-    //         // document.getElementById("loading").innerHTML = "";
-    //         // // document.getElementById("f1").innerHTML = data[0];
-    //         // // document.getElementById("f2").innerHTML = data[1];
-    //         // // document.getElementById("f3").innerHTML = data[2];
-    //     });
-    // });
+    $(document).ready(function() {
+        $.get(localhost, {"url": url}, function(data){
+            // document.getElementById("loading").innerHTML = "";
+            // // document.getElementById("f1").innerHTML = data[0];
+            // // document.getElementById("f2").innerHTML = data[1];
+            // // document.getElementById("f3").innerHTML = data[2];
+            document.getElementById("heroDiv").querySelectorAll("#hero")[0].innerHTML = data[0];
+            document.getElementById("villainDiv").querySelectorAll("#villain")[0].innerHTML = data[1];
+            document.getElementById("victimDiv").querySelectorAll("#victim")[0].innerHTML = data[2];
+        });
+    });
 };
 
 chrome.tabs.query({
