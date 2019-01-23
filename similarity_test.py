747de45e586ed7732eeaf44ac7b71df04da30d45
meganzhao <zhaoxiazhao.megan@gmail.com>
2019-01-22 23:25:33 -0600

    Cut down similar dictionary words

--- similarity_test.py
+++ similarity_test.py
@@ -1,6 +1,9 @@
 from nltk.corpus import wordnet
 import sys
+from role_dictionaries import *
 
+
+#scripts for command line argument 
 list_w1 = wordnet.synsets(sys.argv[1])
 list_w2 = wordnet.synsets(sys.argv[2])
 score = 0
@@ -10,3 +13,24 @@ for w1 in list_w1:
         if cur_score:
             score = max(score, cur_score)
 print(score)
+'''
+result = []
+for word1 in VILLAIN_DICT:
+    for word2 in VILLAIN_DICT:
+        if word1 != word2:
+            list_w1 = wordnet.synsets(word1)
+            list_w2 = wordnet.synsets(word2)
+            score = 0
+            for w1 in list_w1:
+                for w2 in list_w2:
+                    cur_score = w1.wup_similarity(w2)
+                    if cur_score:
+                        if cur_score == 1:
+                            score = cur_score
+                            break
+                        else:
+                            score = max(score, cur_score)
+            if score == 1:
+                result.append((word1, word2))
+print(result)
+'''
