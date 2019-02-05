47cce1ded956275c08c1937b968120fd1aae42fd
Quinn Mayville <mayvilleq@carleton.edu>
2019-02-05 10:11:01 -0600

    added preprocessing of top 10000 words

---
+++ word_similarity_dictionary.py
@@ -0,0 +1,38 @@
+import json
+# from top_words import TOP_WORDS
+from role_assignment import word_similarity, skip_word, HERO, VILLAIN, VICTIM
+from role_dictionaries import HERO_DICT, VILLAIN_DICT, VICTIM_DICT
+
+
+with open('google-10000-english-usa.txt') as input_file:
+
+    dic = {}
+    i = 0
+    for line in input_file:
+        word = line.strip()
+        if not skip_word(word, None):
+            scores = [0, 0, 0]
+            dict_length = len(HERO_DICT)
+            for hero_term in HERO_DICT:
+                scores[HERO] += word_similarity(word, hero_term)
+            scores[HERO] /= len(HERO_DICT)
+            for victim_term in VICTIM_DICT:
+                scores[VICTIM] += word_similarity(word, victim_term)
+            scores[VICTIM] /= len(VICTIM_DICT)
+            for villain_term in VILLAIN_DICT:
+                scores[VILLAIN] += word_similarity(word, villain_term)
+            scores[VILLAIN] /= len(VILLAIN_DICT)
+
+            dic[word] = scores
+        # Print progress
+        i += 1
+        if i % 250 == 0:
+            print(i, "words read...")
+
+
+with open('similarity_dictionary.py', 'w') as output_file:
+    output_file.truncate()
+    output_file.write('SIM_DIC = ')
+    json.dump(dic, output_file)
+
+print("DONE")
