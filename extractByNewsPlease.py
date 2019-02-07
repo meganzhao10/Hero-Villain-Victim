126ff0aff27522f3a7a675c18b7537471141336f
yingyingww <wangc2@carleton.edu>
2019-02-07 16:58:24 -0600

    extract test

--- extractByNewsPlease.py
+++ extractByNewsPlease.py
@@ -5,12 +5,51 @@
 #requires both newspaper and beautifulsoup
 
 from newsplease import NewsPlease
+import urllib.request
 
-url = input("Enter a website to extract the URL's from: ")
+url = "https://www.nbcnews.com/news/religion/ungodly-abuse-lasting-torment-new-tribes-missionary-kids-n967191"
 
-article = NewsPlease.from_url('url')
 
-headline = article.title
-content = article.text
+content = NewsPlease.from_url(url)
+
+
+
+
+#content = NewsPlease.from_url(url)
+headline = content.title
+article = content.text
+
 print(headline)
-print(content)
+print(article)
+
+
+
+
+'''
+NewPlease (usually takes longer time) works for:
+BBC
+reuters
+news.yahoo.com
+huffingtonpost
+https://www.latimes.com/
+https://www.usatoday.com/
+https://www.theguardian.com/us
+https://www.washingtonpost.com/?reload=true
+https://www.dailymail.co.uk/ushome/index.html
+nbc
+
+Partially works for:
+NY Times partial 
+CNN (read more thing)
+wsj: mainly because subsribtion restriction
+
+
+
+
+NOT WORK FOR:
+https://abcnews.go.com/
+usNEWS
+
+
+Cookie handling : Some websites need cookie handling. At the moment the only work around is to use the raw_html extraction. For instance ;
+'''
