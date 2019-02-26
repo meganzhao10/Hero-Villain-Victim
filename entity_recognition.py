25c402f4e39fb299cb46686164d4b526c9d4009e
Quinn Mayville <mayvilleq@carleton.edu>
2019-02-25 20:44:38 -0600

    fixed image bug

--- entity_recognition.py
+++ entity_recognition.py
@@ -199,7 +199,7 @@ def select_high_score_entities(alpha, entity_list, num_sentences):
         elif score > third:
             third = score
             result[2] = entity
-    return result
+    return [x for x in result if x is not None]
 
 
 def get_headline_entities(headline, merged_entities):
@@ -230,14 +230,23 @@ def get_top_entities(headline, tokenized_article):
     # url = input("Enter a website to extract the URL's from: ")
     # print('Headline: ', headline)
     temp_entities, num_sentences = extract_entities_article(tokenized_article)
-    merged_entities = merge_entities(temp_entities)
+    merged_entities_ = merge_entities(temp_entities)
+
+    # Filter out images (entity recognizer thinks images are entities)
+    merged_entities = []
+    for entity in merged_entities_:
+        if "image" not in entity.name.lower():
+            merged_entities.append(entity)
+
     get_headline_entities(headline, merged_entities)
+
     '''
     print('Merged Entities:')
     for e in merged_entities:
         print(e)
     print('------------------------')
     '''
+
     highest_score_entities = select_high_score_entities(0.5, merged_entities, num_sentences)
     headline_entities = [e for e in merged_entities if e.headline and e not in highest_score_entities]
     top_entities = highest_score_entities + headline_entities
