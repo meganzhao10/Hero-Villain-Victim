3d273f5db42b0c400b39593d416123bcdf897779
meganzhao <zhaoxiazhao.megan@gmail.com>
2019-02-27 19:49:27 -0600

    Organize files and add stop words

---
+++ helper_files/role_assignment_one_role_only.py
@@ -0,0 +1,192 @@
+from nltk.corpus import wordnet as wn
+from entity_recognition import get_top_entities, extract_article
+from role_dictionaries import *
+
+# pip3 install textblob
+from textblob import TextBlob
+
+# pip3 install news-please
+# pip3 install newspaper3k
+from newsplease import NewsPlease
+#from newspaper import Article
+
+# pip install beautifulsoup4
+# pip install lxml
+# pip install html5lib
+#from bs4 import BeautifulSoup
+
+def extract_by_newsplease(url):
+    content = NewsPlease.from_url(url)
+    headline = content.title
+    article = content.text
+    return headline, article
+
+'''
+def extract_by_newspaper(url):
+    content = Article(url)
+    content.download()
+    content.parse()
+    headline = content.title
+    article = content.text
+    return headline, article
+
+def extract_by_soup(url):
+    content = BeautifulSoup(url, "lxml")
+    headline = content.title.string
+    articleList = list()
+    for i in content.find_all("p"):
+        articleList.append(i.get_text())
+        # print(i.get_text())
+    return headline, articleList  # TODO modify output so article is string
+'''
+
+def word_similarity(word_1, word_2):
+    '''
+    Returns the Wu-Palmer similarity between the given words.
+    Values range between 0 (least similar) and 1 (most similar).
+    '''
+    syns_w1 = wn.synsets(word_1)
+    syns_w2 = wn.synsets(word_2)
+    score = 0
+    for w1 in syns_w1:
+        for w2 in syns_w2:
+            cur_score = w1.wup_similarity(w2)
+            if cur_score:
+                score = max(score, cur_score)
+    return score
+
+def decay_function(decay_factor, entity_location, term_index):
+    distance = abs(term_index - entity_location[0])
+    if len(entity_location) > 1:
+        distance = min(distance, abs(term_index - entity_location[1]))
+    return (1 - decay_factor) ** distance
+
+
+def sentiment(word):
+    '''
+    Returns the sentiment of the given string as a float within
+    the range [-1.0, 1.0]
+    '''
+    word_blob = TextBlob(word)
+    return word_blob.sentiment.polarity
+
+
+def choose_role(word):
+    '''
+    Uses the sentiment score of a term to determine which dictionary is likely
+    to be most useful.
+    '''
+    s = sentiment(word)
+    if s > 0:
+        return "hero"
+    elif s < 0:
+        return "villain"
+    else:
+        return "all"
+
+
+def similarity_to_role(word, role):
+    similarity_total = 0
+    if role == "hero":
+        dict_length = len(HERO_DICT)
+        for hero_term in HERO_DICT:
+            similarity_total += word_similarity(word, hero_term) / dict_length
+    elif role == "villain":
+        dict_length = len(VILLAIN_DICT)
+        for villain_term in VILLAIN_DICT:
+            similarity_total += word_similarity(word, villain_term) / dict_length
+    elif role == "victim":
+        dict_length = len(VICTIM_DICT)
+        for victim_term in VICTIM_DICT:
+            similarity_total += word_similarity(word, victim_term) / dict_length
+    return similarity_total
+
+
+def role_score_by_sentence(entity, role, index, entity_location, article):
+    '''
+    Calculates the role score of the entity in the given sentence.
+    entity_location is a list where the elements are the beginning and ending indices
+    '''
+    total_score = 0
+    # article from a string to a list of sentences
+    article = extract_article(article)
+    sentence = article[index]
+    begin_index = entity_location[0]
+    end_index = entity_location[1] if len(entity_location) > 1 else entity_location[0]
+    for i in range(len(sentence)):
+        cur_score = 0
+        if not begin_index <= i <= end_index:
+            term_role = choose_role(sentence[i])
+            if term_role == role or term_role == "all":
+                cur_score += similarity_to_role(sentence[i], role)
+                # cur_score += additional_score(entity, role, sentence[i])
+                cur_score *= decay_function(0.5, entity_location, i)
+        total_score += cur_score
+    return total_score
+
+
+def entity_role_score(entity, role, article):
+    '''
+    Calculates the role score of the entity by averaging the
+    role scores of the sentences where the entity appears.
+    '''
+    sentences = entity.locations
+    total_score = 0
+    count = 0
+    for index in sentences:
+        total_score += role_score_by_sentence(entity, role, index, sentences[index], article)
+        count += 1
+    print(role + ": " + str(total_score/count))
+    return total_score / count
+
+
+def main(url):
+    '''
+    Retrieve the three top entities from entity_recognition.py;
+    assign each entity the role with the highest role score.
+    '''
+    headline, article = extract_by_newsplease(url)
+    entities = get_top_entities(headline, article)
+    scores = {}
+    assignments = {}
+    '''
+    scores[("bob", "hero")] = 0.4
+    scores[("bob", "villain")] = 0.5
+    scores[("bob", "victim")] = 0.2
+
+    scores[("ds", "villain")] = 0.9
+    scores[("ds", "hero")] = 0.8
+    scores[("ds", "victim")] = 0.7
+    
+    scores[("ALICE", "villain")] = 0.89
+    scores[("ALICE", "hero")] = 0.5
+    scores[("ALICE", "victim")] = 0.7
+    #DS:VILLAIN; ALICE: VICTIM; BOB: HERO
+    '''
+    # Calculate all three scores for each entity and add to dictionary scores
+    for entity in entities:
+        hero_score = entity_role_score(entity, "hero", article)
+        villain_score = entity_role_score(entity, "villain", article)
+        victim_score = entity_role_score(entity, "victim", article)
+        scores[(entity, "hero")] = hero_score
+        scores[(entity, "villain")] = villain_score
+        scores[(entity, "victim")] = victim_score
+
+    # Sort scores in descending order and assign roles
+    sorted_scores = sorted(scores, key = scores.get, reverse = True)
+    for w in sorted_scores:
+        if w in scores:
+            role_to_assign = w[1]
+            entity_to_assign = w[0]
+            assignments[role_to_assign] = (entity_to_assign, scores[w])
+            for entity, role in list(scores):
+                if (role == role_to_assign or entity == entity_to_assign) and \
+                        (entity, role) in scores:
+                    del scores[(entity, role)]             
+    for role in assignments:
+        print(role + ": " +  assignments[role][0].name + " " + str(assignments[role][1]))
+    return entities
+
+
+if __name__ == "__main__":
+    main("https://www.npr.org/2019/01/29/689581417/apple-disables-group-facetime-after-security-flaw-let-callers-secretly-eavesdrop")
---
+++ helper_files/test_significant_score.py
@@ -0,0 +1,81 @@
+from role_assignment import similarity_to_role
+from role_dictionaries import HERO_DICT, VILLAIN_DICT, VICTIM_DICT
+
+# Constants to represent roles
+HERO = 0
+VILLAIN = 1
+VICTIM = 2
+
+# HERO_DICT = {'gentle', 'preserving', 'leadership', 'amazing', 'devoted', 'humble', 'warned', 'surprised', 'humanity', 'brave', 'evacuate', 'redemption', 'smile', 'honor', 'revolutionize', 'leader', 'advocate', 'savior', 'charity', 'sympathies', 'kindness', 'good', 'protect', 'teach', 'reputation', 'respected', 'welfare', 'glory', 'victory', 'winner', 'well', 'contained', 'restoration', 'commitment', 'ability', 'efforts', 'inspire', 'safety', 'allies', 'health', 'strength', 'empowered', 'passion', 'encouraging', 'warm', 'vision', 'scored', 'authorities', 'justice', 'grand', 'admire', 'reshape', 'communities', 'response', 'strengthen', 'bolster', 'intervened', 'motivated', 'reconstruct', 'freedom', 'duty', 'aided', 'conquer', 'smart', 'bravery', 'improve', 'donate', 'wise', 'ingenuity', 'milestone', 'protections', 'expand', 'hero', 'pursuit', 'invent', 'containment', 'achievement', 'supporters'}
+# VILLAIN_DICT = {'contaminate', 'dirty', 'abduct', 'terror', 'worsen', 'crisis', 'lambast', 'abandonment', 'harass', 'subvert', 'virus', 'crime', 'provoke', 'kidnap', 'manipulate', 'alleged', 'refusal', 'trafficking', 'marginalize', 'conformity', 'clampdown', 'villain', 'disparaged', 'cold', 'exacerbate', 'alienate', 'commit', 'trial', 'violence', 'denounced', 'stripped', 'undermine', 'seize', 'persecuted', 'opposing', 'intimidate', 'jailed', 'fool', 'investigation', 'imprisoned', 'bias', 'deception', 'gunshots', 'threaten', 'hoax', 'engulfed', 'blame', 'eruption', 'offensive', 'contempt', 'suggested', 'coercion', 'erase', 'catastrophe', 'rumors', 'weaken', 'pointed', 'treason', 'evil', 'abused', 'sentenced', 'bullet', 'warn', 'devastate', 'convicted', 'rebuke', 'reveal', 'bully', 'collude'}
+# VICTIM_DICT = {'setback', 'injured', 'traumatized', 'prevented', 'healing', 'buried', 'stuck', 'anguished', 'flee', 'suffer', 'casualty', 'trampled', 'forsaken', 'harassed', 'harassment', 'hardship', 'deported', 'howling', 'shocked', 'violence', 'depressed', 'danger', 'mute', 'stripped', 'terrified', 'distrust', 'assassinated', 'shivering', 'sick', 'complain', 'abducted', 'huddled', 'victimized', 'persecuted', 'barricaded', 'devastated', 'kidnapped', 'seized', 'justified', 'evacuated', 'surrendered', 'diagnosed', 'imprisoned', 'independence', 'slave', 'deceased', 'rebuffed', 'target', 'trapped', 'screamed', 'loss', 'trafficked', 'humiliated', 'impairment', 'wounded', 'discriminated', 'disadvantaged', 'blood', 'offended', 'accuses', 'saddens', 'threatened', 'disaster', 'devastation', 'overshadowed', 'tortured', 'abused', 'remonstrated', 'jeopardizing', 'stabbed', 'prey', 'sentenced', 'challenged', 'renounced', 'scared', 'humiliation', 'deaths', 'rescued', 'bleeding'}
+
+zero_score_words = []
+
+# count zero score word? but only for full dictionary
+def calculate_score(dict, role):
+	# print('---------')
+	total_score = 0
+	for word in dict:
+		total_score += similarity_to_role(word, role)
+		# if cur == 0:
+		# 	print(word)
+	return total_score / len(dict)
+
+
+print("HERO - HERO: " + str(calculate_score(HERO_DICT, HERO)))
+print("VILLAIN - VILLAIN: " + str(calculate_score(VILLAIN_DICT, VILLAIN)))
+print("VICTIM - VICTIM: " + str(calculate_score(VICTIM_DICT, VICTIM)))
+
+print("----------------------------------------")
+print("HERO - VILLAIN: " + str(calculate_score(HERO_DICT, VILLAIN)))
+print("VILLAIN - HERO: " + str(calculate_score(VILLAIN_DICT, HERO)))
+print("VICTIM - HERO: " + str(calculate_score(VICTIM_DICT, HERO)))
+print("HERO - VICTIM: " + str(calculate_score(HERO_DICT, VICTIM)))
+
+
+
+'''
+keep changing standardization
+
+Full dictionary 10k
+HERO - HERO: 0.43593697829454453
+VILLAIN - VILLAIN: 0.45674700531749385
+VICTIM - VICTIM: 0.4703535636156092
+----------------------------------------
+HERO - VILLAIN: 0.4246066886216203
+VILLAIN - HERO: 0.3187238823521552
+VICTIM - HERO: 0.27248955419349413
+HERO - VICTIM: 0.43848298826863097
+
+Full dictionary 100k
+HERO - HERO: 0.9639838467566706
+VILLAIN - VILLAIN: 0.9759726872820005
+VICTIM - VICTIM: 0.9676917967296632
+----------------------------------------
+HERO - VILLAIN: 0.9521711059837578
+VILLAIN - HERO: 0.8806906494929407
+VICTIM - HERO: 0.8392620609487662
+HERO - VICTIM: 0.9523609127801426
+
+Filtered 10k
+HERO - HERO: 0.21899435832908964
+VILLAIN - VILLAIN: 0.2913825463399934
+VICTIM - VICTIM: 0.4146205376653103
+----------------------------------------
+HERO - VILLAIN: 0.19371338720301848
+VILLAIN - HERO: -0.09617151620820986
+VICTIM - HERO: -0.10662036598042332
+HERO - VICTIM: 0.1646791386903146
+
+Filtered 100k
+HERO - HERO: 0.8302747246712505
+VILLAIN - VILLAIN: 0.869050013303774
+VICTIM - VICTIM: 0.9367848968832265
+----------------------------------------
+HERO - VILLAIN: 0.7936012155642856
+VILLAIN - HERO: 0.5990784437051674
+VICTIM - HERO: 0.5914134793074337
+HERO - VICTIM: 0.74802459936
+
+'''
