93e147161a78cdda34a0eda950cd57ab2d59a7a9
Quinn Mayville <mayvilleq@carleton.edu>
2019-03-12 19:08:58 -0500

    removed unused file

--- neg_words.py
+++
@@ -1,8 +0,0 @@
-NEG_WORDS = ["not", "never","no",
-             "neither","nor","none","nobody","nothing","nowhere",
-             "hardly","seldom" "scarcely","barely", "wasn't","weren't","isn't","aren't",
-             "cannot","can't","couldn't",
-             "doesn’t","don't","didn't",
-             "hasn't","haven't","hadn't",
-             "won't","wouldn't",
-             "shouldn't"]
